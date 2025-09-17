81c956501176def7759894955549752ba2da9acc
unsoul7 <disketteit@gmail.com>
2025-09-17 10:32:14 +0530

    completed admin panel

---
+++ app.js
@@ -0,0 +1,4 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js";
+import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-firestore.js";
+
+// Firebase config (replace with your real config)
